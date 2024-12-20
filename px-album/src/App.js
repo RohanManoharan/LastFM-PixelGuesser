c11184819907307f990d4d1b3b23f0fec4b06c21
RohanManoharan <rohan.manoharan@ryerson.ca>
2024-12-19 22:22:56 -0500

    lastFM api auth working

--- px-album/src/App.js
+++ px-album/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+const apikey = process.env.REACT_APP_LASTFM_API_KEY;
+const lastFmAuthUrl = `http://www.last.fm/api/auth/?api_key=${apikey}&cb=http://example.com`;
+
+const handleLogin = () => {
+  window.location.href = lastFmAuthUrl;
+};
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Pixel Album Game</h1>
+      <button onClick={handleLogin}>Login with Last.fm</button>
+
+
     </div>
   );
 }
