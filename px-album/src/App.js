235913e20f83ce06ec7c1d92820d598376008fbb
RohanManoharan <rohan.manoharan@ryerson.ca>
2024-12-23 21:44:17 -0500

    Update App.js

--- px-album/src/App.js
+++ px-album/src/App.js
@@ -1,27 +1,52 @@
 import './App.css';
+import axios from 'axios';
 import crypto from 'crypto-js';
 
+// LAST FM ROOT: http://ws.audioscrobbler.com/2.0/
 const apiKey = process.env.REACT_APP_LASTFM_API_KEY;
 const apiSecret = process.env.REACT_APP_LASTFM_SECRET;
 
+//Login
 const handleLogin = () => {
   const lastFmAuthUrl = `http://www.last.fm/api/auth/?api_key=${apiKey}&cb=http://localhost:3000`;
   window.location.href = lastFmAuthUrl;
 };
 
+// Get Token
 const params = new URLSearchParams(window.location.search);
-const userToken = params.get('token');
+const token = params.get('token');
 
-console.log("ApiKey "+apiKey);
-console.log("userToken "+userToken);
-console.log("ApiSecret "+apiSecret);
-
+// Hash apiSig
 const apiSig = crypto.MD5(
-  `api_key${apiKey}methodauth.getSessiontoken${userToken}${apiSecret}`
+  `api_key${apiKey}methodauth.getSessiontoken${token}${apiSecret}`
 ).toString();
 
 
+// Testing
+console.log("ApiKey "+apiKey);
+console.log("ApiSecret "+apiSecret);
+console.log("token "+token);
 console.log("Apisig "+apiSig);
+
+// Get user's session key -- Turns out this is not going to be used but I will here for now...
+axios.get(`http://ws.audioscrobbler.com/2.0/?method=auth.getSession&api_key=${apiKey}&format=json&api_sig=${apiSig}&token=${token}`)
+  .then(function (res) {
+    const sk = res.data.session.key;
+    const userName = res.data.session.name;
+    console.log(sk)
+  }).catch(err => {
+    console.log(err.response);
+  });
+
+//
+  axios.get(`http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${userName}&api_key=${apiKey}&format=json`)
+  .then(function (res) {
+    const sk = res.data.session.key;
+    console.log(sk)
+  }).catch(err => {
+    console.log(err.response);
+  });
+
 
 function App() {
   return (
