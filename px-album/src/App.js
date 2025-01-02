1221a490ca39a3ed473b32145a84d9426a67eff0
RohanManoharan <rohan.manoharan@ryerson.ca>
2025-01-02 18:29:16 -0500

    can pixelate albums

--- px-album/src/App.js
+++ px-album/src/App.js
@@ -66,13 +66,50 @@ function App() {
     }
   }
 
+  function pixelateImage(){
+    let c = document.createElement("canvas");
+    let img1 = new Image();
+    img1.crossOrigin = "Anonymous";
+
+
+    img1.onload = function(){
+      document.getElementById("ab-img").remove();
+      let w = img1.width;
+      let h = img1.height;
+
+      c.width = w;
+      c.height = h;
+      let ctx = c.getContext("2d");
+      ctx.drawImage(img1,0,0);
+
+      let pixelArr = ctx.getImageData(0,0,w,h).data;
+      let sample_size=20;
+
+      for (let y=0;y<h;y+=sample_size)
+        for(let x=0;x<w;x+=sample_size){
+      let p = (x+(y*w))*4;
+      ctx.fillStyle = "rgba(" + pixelArr[p] + "," + pixelArr[p+1] + "," + pixelArr[p+2] + "," + pixelArr[p+3] + ")";
+      ctx.fillRect(x,y,sample_size,sample_size);
+    }
+
+
+      let img2  = new Image();
+      img2.src = c.toDataURL("image/jpeg");
+      img2.width = w;
+      document.body.appendChild(img2);
+    }
+    img1.src = document.getElementById('ab-img').src;
+  }
+
   return (
     <div>
       <h1>Pixel Album Game</h1>
       <button onClick={handleLogin}>Login with Last.fm</button>
-      <p id='ab-name'>{albumName}</p>
+      <p id='ab-name'>{albumName} </p>
       <img id='ab-img' src={albumImg} alt={albumName} /> <br></br>
       <button onClick={generateRandomAlbum}>Generate</button>
+      <button onClick={pixelateImage}>Pixelate</button>
+
       <p id="genAlbumError"></p>
     </div>
   );
