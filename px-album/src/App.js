760c5e153cd47eed9dcdd44eaf2e06448b30275b
RohanManoharan <rohan.manoharan@ryerson.ca>
2025-01-09 21:44:04 -0500

    changed callback

--- px-album/src/App.js
+++ px-album/src/App.js
@@ -13,7 +13,7 @@ const apiSecret = process.env.REACT_APP_LASTFM_SECRET;
 
 //Login
 const handleLogin = () => {
-  const lastFmAuthUrl = `http://www.last.fm/api/auth/?api_key=${apiKey}&cb=http://localhost:3000`;
+  const lastFmAuthUrl = `http://www.last.fm/api/auth/?api_key=${apiKey}&cb=http://https://rohanmanoharan.github.io/LastFM-PixelGuesser/`;
   window.location.href = lastFmAuthUrl;
 };
 
